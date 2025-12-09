c266e600e66ed1cd76aebb09edf22f1b153a2e6c
Dayoung <dayoung.kim@lincolnuni.ac.nz>
2025-12-09 14:27:27 +1300

    Flask tutorial

---
+++ app.py
@@ -0,0 +1,55 @@
+
+
+import re
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flask!"
+
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello_there(name = None):
+#     return render_template(
+#         "hello_there.html",
+#         name=name,
+#         date=datetime.now()
+#     )
+
+# Replace the existing home function with the one below
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+
+# @app.route("/hello/<name>")
+# def hello_there(name):
+#     now = datetime.now()
+#     formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+#     # Filter the name argument to letters only using regular expressions. URL arguments
+#     # can contain arbitrary text, so we restrict to safe characters only.
+#     match_object = re.match("[a-zA-Z]+", name)
+
+#     if match_object:
+#         clean_name = match_object.group(0)
+#     else:
+#         clean_name = "Friend"
+
+#     content = "Hello there, " + clean_name + "! It's " + formatted_now
+#     return content
